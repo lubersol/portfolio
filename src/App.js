ace5d737971cfb42507f5d5297191e699b52528b
Lucia <lubersol@gmail.com>
2021-03-25 13:59:36 +0100

    adding routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
 
+
+//Components
+import Contacto from './components/Contacto/Contacto';
+import Educacion from './components/Educacion/Educacion';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Portfolio from './components/Portfolio/Portfolio';
+import SobreMi from './components/SobreMi/SobreMi.js';
+
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Header />
+      <SobreMi />
+      <Portfolio />
+      <Educacion />
+      <Contacto />
     </div>
   );
 }
