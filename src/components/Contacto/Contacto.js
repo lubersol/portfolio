66ee06c6cbbbf732b623e177c465e8e16f770277
Lucia <lubersol@gmail.com>
2021-03-30 13:37:02 +0200

    creating components Education, Footer, Contacto..

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,7 +1,51 @@
-// import React, { Component } from 'react'
-// export default class Contacto extends Component {
-//   render() {
-//     return (
-//     )
-//   }
-// }
+import React, { Component } from 'react'
+export default class Contacto extends Component {
+    render() {
+        return (
+            <>
+                <section id="contact" className="s-contact target-section">
+                    <div className="row s-contact__header">
+                        <div className="column large-12">
+                            <h3 className="section-header-allcaps">Say Hello</h3>
+                        </div>
+                    </div>
+                    <div className="row s-contact__content">
+                        <div className="column large-7 medium-12">
+                            <h4 className="huge-text">
+                                Have a new project in mind? Let's collaborate and build something awesome.
+                                Let's turn that idea to an even greater product :)
+                            </h4>
+                        </div>
+                        <div className="column large-4 medium-12">
+                            <div className="row contact-infos">
+                                <div className="column large-12 medium-6 tab-12">
+                                    <div className="contact-block">
+                                        <h5 className="contact-block__header">
+                                            Email
+                                        </h5>
+                                        <p className="contact-block__content">
+                                            <a className="mailtoui" href="mailto:lubersol@gmail.com">lubersol@gmail.com</a>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="column large-12 medium-6 tab-12">
+                                    <div className="contact-block">
+                                        <h5 className="contact-block__header">
+                                            Móvil
+                                        </h5>
+                                        <p className="contact-block__content">
+                                            <a href="tel:+34696925484">+34 696 925 484</a>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="column large-12">
+                                    <a href="mailto:sayhello@ceevee.com" className="mailtoui btn btn--primary h-full-width">Let's Talk</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </>
+        )
+    }
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,22 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
         return (
-            <header id="home" className="s-header">
+            <div id="home" className="s-header">
                 <div className="row s-header__nav-wrap">
                     <nav className="s-header__nav">
                         <ul>
                             <li className="current"><a className="smoothscroll" href="#hero">Home</a></li>
-                            <li><a className="smoothscroll" href="#sobremi">Sobre mí</a></li>
-                            <li><a className="smoothscroll" href="#educacion">Educacion</a></li>
-                            <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
-                            <li><a className="smoothscroll" href="#contacto">Contacto</a></li>
+                            <li><a className="smoothscroll" href="#SobreMi">Sobre mí</a></li>
+                            <li><a className="smoothscroll" href="#Educacion">Educacion</a></li>
+                            <li><a className="smoothscroll" href="#Portfolio">Portfolio</a></li>
+                            <li><a className="smoothscroll" href="#Contacto">Contacto</a></li>
                         </ul>
                     </nav>
                 </div> 
                 <a className="s-header__menu-toggle" href="#0" title="Menu">
                     <span className="s-header__menu-icon" />
                 </a>
-            </header> 
+            </div> 
     )
     }
 }
